f6702f78824a341d5285707f7ad7a87bafb13635
Tariq Abdulghnai <tariq.abdulghani.hussein@gmail.com>
2021-09-24 17:44:22 +0200

    closes #22 added specialty and its association to vet

---
+++ pet-clinic-data/src/main/java/com/example/petclinic/model/Specialty.java
@@ -0,0 +1,16 @@
+package com.example.petclinic.model;
+
+public class Specialty extends BaseEntity{
+
+	private String Description;
+
+	public String getDescription() {
+		return Description;
+	}
+
+	public void setDescription(String description) {
+		Description = description;
+	}
+	
+	
+}
