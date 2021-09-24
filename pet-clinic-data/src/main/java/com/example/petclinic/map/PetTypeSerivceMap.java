7b596ce86897dcd4e6f3c9303c36970ec4577f3e
Tariq Abdulghnai <tariq.abdulghani.hussein@gmail.com>
2021-09-24 19:14:56 +0200

    closes #25 added pet type service map

---
+++ pet-clinic-data/src/main/java/com/example/petclinic/map/PetTypeSerivceMap.java
@@ -0,0 +1,8 @@
+package com.example.petclinic.map;
+
+import com.example.petclinic.model.PetType;
+import com.example.petclinic.service.PetTypeService;
+
+public class PetTypeSerivceMap extends AbstractMapService<Long, PetType> implements PetTypeService{
+
+}
